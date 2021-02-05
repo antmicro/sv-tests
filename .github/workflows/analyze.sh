82b8cbf3127a6740906f47b47428548c38b55cd9
Pawel Czarnecki <pczarnecki@antmicro.com>
2021-02-05 19:42:26 +0100

    only activate conda env

--- .github/workflows/analyze.sh
+++ .github/workflows/analyze.sh
@@ -9,17 +9,10 @@ CHANGES_SUMMARY=$OUT_DIR"/"$JOB_NAME"_changes_summary.json"
 set -x
 set -e
 
-# Get a conda environment
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
 source "$HOME/miniconda/etc/profile.d/conda.sh"
 hash -r
-conda config --set always_yes yes --set changeps1 no
-
-conda env create --file conf/environment.yml
 conda activate sv-test-env
 hash -r
-conda info -a
 
 # Get base report from sv-tests master run
 wget https://symbiflow.github.io/sv-tests-results/report.csv -O $BASE_REPORT
